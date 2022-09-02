b40273b8a127b4538d2a1573fc8fd1d5f236908c
nooblikeU <unnamed444@protonmail.com>
2022-09-02 05:57:55 +0000

    Added icon on dwm bar

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 24;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=11" };
-static const char dmenufont[]       = "Hack Nerd Font:size=11";
+static const char *fonts[]          = { "Hack Nerd Font:size=13" };
+static const char dmenufont[]       = "Hack Nerd Font:size=13";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -22,7 +22,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
